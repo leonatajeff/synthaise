4dfed9a4f32f3f3a8bd2212dcfe2cb829be0eb5a
Jefferson Leonata <leonatajeff@gmail.com>
2023-03-29 01:00:23 -0400

    version 2.0
    cleaning up and adding instructions to work with this

--- README.md
+++ README.md
@@ -1,7 +1,68 @@
-# synthaise
-generative sound effects with ai
+# Synthaise
 
-mono-repo for the model that will run in a seperate service and the web app on another service
+Generative sound effects with AI. This project has pivoted to an AI-powered search through a small sound effect library using the UrbanSound8k dataset.
 
-link to dataset: https://www.kaggle.com/datasets/chrisfilo/urbansound8k
-to run locally run 'python app.py'
+## Dataset
+
+The dataset used in this project can be downloaded from Kaggle at the following link:
+
+[UrbanSound8k Dataset](https://www.kaggle.com/chrisfilo/urbansound8k)
+
+### Instructions to set up the dataset
+
+1. Register or log in to your Kaggle account.
+2. Download the `urbansound8k.zip` file from the link above.
+3. Extract the contents of the `urbansound8k.zip` file.
+4. Place the extracted `UrbanSound8K` folder inside your local repository of the Flask app. The final structure should look like this:
+5. Create a folder `audio-files`, this is where the results will be locally stored.
+
+synthaise/
+├─ client/
+├─ templates/
+├─ static/
+├─ app.py
+├─ UrbanSound8K/
+├─ audio_files/
+├─ .gitignore
+└─ README.md
+
+
+## Running the Flask app locally
+
+1. Make sure you have Python 3.x and `pip` installed on your system.
+2. Navigate to the root folder of the Flask app (`synthaise`).
+3. Create a virtual environment
+
+```bash
+python -m venv venv
+```
+
+then:
+
+On Windows:
+```bash
+venv\Scripts\activate
+```
+
+On macOS and Linux:
+```bash
+source venv/bin/activate
+
+```
+
+5. pip install -r requirements.txt
+6. `flask run` will run the development server.
+
+if step 6 this doesn't work, set your environment variables:
+
+On Windows:
+```bash
+set FLASK_APP=app.py
+```
+
+On macOS and Linux:
+```bash
+export FLASK_APP=app.py
+```
+
+
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-# import machine learning libraries
+from flask import Flask, request, render_template, send_file
 import pandas as pd
 import numpy as np
 import os
 import re
 import nltk
 from nltk.corpus import stopwords
-from gensim.models import Word2Vec, FastText
+from gensim.models import Word2Vec
+
 nltk.download('punkt')
 nltk.download('stopwords')
 stop_words = stopwords.words('english')
@@ -18,8 +19,7 @@ def preprocess_text(text):
     text = nltk.word_tokenize(text)
     return text
 
-# link to dataset https://www.kaggle.com/datasets/chrisfilo/urbansound8k
-df = pd.read_csv('../audio_files/UrbanSound8k.csv')
+df = pd.read_csv('./urbansound8k/UrbanSound8K.csv')
 class_column = df['class']
 audio_names = class_column.tolist()
 class_to_filename = dict(zip(df['class'], df['slice_file_name']))
@@ -31,28 +31,14 @@ for name in audio_names:
     extended_audio_names.append(name.split('_'))
 
 processed_audio_names = [preprocess_text(name) for name in audio_names] + extended_audio_names
-#print(processed_audio_names[:5])
-#print('car' in [word for words in processed_audio_names for word in words])
-#print('car_horn' in [word for words in processed_audio_names for word in words])
+
 # Train a Word2Vec model
 model = Word2Vec(sentences=processed_audio_names, min_count=1, vector_size=100, window=5, workers=4)
 
 # display model vocabulary
 words = list(model.wv.key_to_index)
 
-# display model vocabulary size
-#print(words)
-# print(len(set(words[:5])))
-# print('********')
 
-
-
-def get_mean_vector(words):
-    vectors = [model.wv[word] for word in words if word in model.wv.key_to_index]
-    if vectors:
-        return np.mean(vectors, axis=0)
-    else:
-        return None
 
 def get_similar_words(input_text, topn=3):
     input_words = preprocess_text(input_text)
@@ -71,27 +57,35 @@ def get_similar_words(input_text, topn=3):
     if similar_classes:
         # Select only rows with the similar classes
         similar_rows = df[df['class'].isin(similar_classes)][['slice_file_name', 'fold']]
-        # Convert the rows to a list of tuples (file_name, fold)
-        similar_files = list(similar_rows.itertuples(index=False, name=None))
-        return similar_files[:topn]
+        # Replace any remaining backslashes with forward slashes
+        similar_rows['slice_file_name'] = similar_rows['slice_file_name'].str.replace('\\', '/')
+        # Generate full file path of the audio files
+        audio_files = []
+        for file_name, fold in similar_rows.itertuples(index=False):
+            audio_file_path = os.path.join('audio_files', 'static', f'fold{fold}', file_name)
+            audio_files.append((file_name, audio_file_path))
+        return audio_files[:topn]
     else:
         return []
 
-# def jaccard_similarity(a, b):
-#     a_set = set(a)
-#     b_set = set(b)
-#     intersection = a_set.intersection(b_set)
-#     union = a_set.union(b_set)
-#     return len(intersection) / len(union)
 
-# def get_similar_words(input_text, topn=3):
-#     input_words = preprocess_text(input_text)
-#     scores = [(row.slice_file_name, row['class'], row.fold, jaccard_similarity(input_words, row['class'])) for index, row in df.iterrows()]
-#     sorted_scores = sorted(scores, key=lambda x: x[3], reverse=True)[:topn]
-#     return [(file_name, fold) for file_name, class_name, fold, score in sorted_scores]
 
-# Returns 3 audio files and what fold they are in 
+app = Flask(__name__)
 
-input_text = 'engine idling'
-similar_results = get_similar_words(input_text, 3)
-print(similar_results if similar_results else f"'{input_text}' not found in the dataset.")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_text = request.form['input_text']
+        similar_files = get_similar_words(input_text, topn=3)
+        return render_template('index.html', results=similar_files)
+    else:
+        return render_template('index.html')
+    
+# Define route for serving audio files
+@app.route('/audio_files/<path:file_path>')
+def get_audio(file_path):
+    formatted_file_path = file_path.replace('\\', '/')
+    return send_file(formatted_file_path, mimetype='audio/wav')
+
+if __name__ == '__main__':
+    app.run(debug=True)
